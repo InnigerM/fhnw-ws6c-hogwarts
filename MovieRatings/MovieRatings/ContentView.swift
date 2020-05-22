6b2f019c1f1a06418a9727c082d4edd1e1da8091
InnigerM <marco.inniger@gmail.com>
2020-05-22 10:46:57 +0200

    Initial

---
+++ MovieRatings/MovieRatings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieRatings
+//
+//  Created by Marco Inniger on 22.05.20.
+//  Copyright © 2020 FHNW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
