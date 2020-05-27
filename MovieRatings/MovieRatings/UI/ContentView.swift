89a9037237e867b8302f382160b776b4dcaa56a6
InnigerM <marco.inniger@gmail.com>
2020-05-27 15:16:27 +0200

    Updated Movie Layout

--- MovieRatings/MovieRatings/UI/ContentView.swift
+++ MovieRatings/MovieRatings/UI/ContentView.swift
@@ -5,24 +5,33 @@
 //  Created by Marco Inniger on 22.05.20.
 //  Copyright © 2020 FHNW. All rights reserved.
 //
-
+import Kingfisher
 import SwiftUI
 
 
 struct ContentView: SwiftUI.View {
     
     @ObservedObject var viewModel = MovieViewModel()
+//    var vote : Int;
     
     var body: some SwiftUI.View {
-       VStack {
-        ForEach(viewModel.movies) { movie in
-            HStack{
-                VStack { Text(movie.title) // arange sub view in vertical
-                    HStack { Text(String(format:"%.1f", movie.voteAverage))
-                   }
-                 }
-              }
-          }
-       }
+        List {
+            ForEach(viewModel.movies) { movie in
+                HStack{
+                    KFImage(movie.fullPosterURL)
+                        .cancelOnDisappear(true)
+                        .resizable()
+                        .frame(height: 100)
+                        .frame(width: 100)
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text(movie.title)
+                        Text(String(format:"%.1f", movie.voteAverage))
+//                        RatingView(rating: vote)
+                    }
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+            }
+        }
     }
 }
